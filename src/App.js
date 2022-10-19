b888ab83b38221d89dabe84251984419540eea7a
Ramazan Doğan <doganrmzn40@gmail.com>
2022-10-19 14:26:04 +0300

    Finished CSS and HTML
    Finished CSS and HTML

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import "./App.css";
+import "./styles.css";
 
 function App() {
   return (
-    <div className="calculor-grid">
+    <div className="calculator-grid">
       <div className="output">
         <div className="previous-operand"></div>
         <div className="current-operand"></div>
@@ -14,9 +14,11 @@ function App() {
       <button>1</button>
       <button>2</button>
       <button>3</button>
+      <button>*</button>
       <button>4</button>
       <button>5</button>
       <button>6</button>
+      <button>+</button>
       <button>7</button>
       <button>8</button>
       <button>9</button>
